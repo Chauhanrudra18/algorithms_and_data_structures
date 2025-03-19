deaf1d18aede4c33b58d776139948029352bdd0b
chauhanrudra18 <chauhanrudra0018@gmail.com>
2025-03-19 22:37:25 +0530

    CORRECT HEADER FILE INCLUDED AND THE INCORRECT LINE REMOVED

--- queue_problems/queueDemo.cpp
+++ queue_problems/queueDemo.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
-#include "../include/queue.h"
-#include "../include/generic.h"
+#include <queue>
+#include <cstdlib>
+#include <ctime>
+
+// Function to generate random numbers in a range
+namespace algo {
+    int random_range(int min, int max) {
+        static bool first = true;
+        if (first) {
+            srand(static_cast<unsigned int>(time(nullptr))); // Seed the random number generator
+            first = false;
+        }
+        return min + rand() % ((max + 1) - min);
+    }
+}
 
 
 int main()
 {
     const int QUEUE_SIZE = 10;
-    algo::Queue<int> Q(QUEUE_SIZE);
-
+    std::queue<int> Q;
     std::cout << "Pushing following values to queue:\n";
     for ( int i = 0; i < QUEUE_SIZE; ++i )
     {
@@ -16,8 +28,8 @@ int main()
         Q.push(rand_value);
     }
     std::cout << std::endl;
-    std::cout << "Size of Queue is :" << Q.count() << std::endl;
-    std::cout << "\nPopping queue values :\n";
+    // std::cout << "Size of Queue is :" << Q.count() << std::endl; // Incorrect line, removed
+    std::cout << "Size of Queue is :" << Q.size() << std::endl;
     while ( !Q.empty() ) {
         std::cout << Q.front() << " ";
         Q.pop();
